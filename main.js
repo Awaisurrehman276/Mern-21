116e966c2f332277ca084686db6734de86108358
Awaisurrehman276 <aur276@gmail.com>
2024-07-12 07:11:20 -0700

    7/12/2024

--- main.js
+++ main.js
@@ -1,12 +1,48 @@
-x= 964;
-console.log("Hello world");
+// Case 1: When we intialize value without the data type in Block 
+let awais= 94;
+console.log(awais);
+{
+    awais=81;
+    console.log(awais);
+}
+console.log(awais);
 
-<<<<<<< HEAD
-x= 352623623;
-console.log("Hello world test");
-=======
+// Case 2: When we intialize value with the data type in block (reassign values)
+let a=109;
+console.log(a);
+{
+    let a=115;
+    console.log(a);
+}
+a=1;
+console.log(a);
 
+// Case 3: When we redefine variable names( Shows error as LET don't allow to redefine same variable name)
+//let a=109;
+console.log(a);
+{
+    let a=115;
+    console.log(a);
+}
+a=1;
+console.log(a);
+//
+//Case 1(Const) Where we reassigned value of Const in block
+const Age=24;
+console.log(Age);
+{
+    const Age= 9;
+    console.log(Age);
+}
+console.log(Age);
+//Case 2(Const) Where we used no value of const in the block
+const Age1=61;
+console.log(Age1);
+{
+    
+    console.log(Age1);
+}
+console.log(Age1);
 
-x= 964;
-console.log("Hello world");
->>>>>>> function/conflict
+//
+
